96fad745a7284ddf2c995fe633b6a06dd8d9ea1f
Tomáš Reinert <reinerttomas@gmail.com>
2023-05-15 14:03:36 +0200

    profile tests

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -25,6 +25,18 @@ class ProfileTest extends TestCase
             ]);
     }
 
+    public function testNotAuthenticatedUserCannotGetProfile(): void
+    {
+        $response = $this->getJson('/api/v1/profile');
+
+        $response->assertStatus(401)
+            ->assertJsonStructure(['message'])
+            ->assertJsonCount(1)
+            ->assertJson([
+                'message' => 'Unauthenticated.'
+            ]);
+    }
+
     public function testUserCanUpdateNameAndEmail(): void
     {
         $user = User::factory()->create();
@@ -48,6 +60,48 @@ class ProfileTest extends TestCase
         ]);
     }
 
+    public function testUserCannotUpdateWithEmptyBody(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/profile');
+
+        $response->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonCount(2)
+            ->assertJsonFragment([
+                'errors' => [
+                    'name' => [
+                        'The name field is required.'
+                    ],
+                    'email' => [
+                        'The email field is required.'
+                    ],
+                ]
+            ]);
+    }
+
+    public function testUserCannotUpdateWithNotValidEmail(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/profile', [
+            'name' => 'John Doe',
+            'email' => 'john.doe'
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonCount(2)
+            ->assertJsonFragment([
+                'errors' => [
+                    'email' => [
+                        'The email field must be a valid email address.'
+                    ],
+                ]
+            ]);
+    }
+
     public function testUserCanChangePassword(): void
     {
         $user = User::factory()->create();
@@ -59,5 +113,65 @@ class ProfileTest extends TestCase
         ]);
 
         $response->assertStatus(202);
+    }
+
+    public function testUserCannotChangePasswordWithWrongCurrentPassword(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/password', [
+            'current_password' => 'wrong-password',
+            'password' => 'password123!',
+            'password_confirmation' => 'password123!',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonCount(2)
+            ->assertJsonValidationErrors([
+                'current_password' => [
+                    'password' => [
+                        'The password is incorrect.'
+                    ],
+                ]
+            ]);
+    }
+
+    public function testUserCannotChangePasswordWithPasswordConfirmationNotMatching(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/password', [
+            'current_password' => 'password',
+            'password' => 'password123',
+            'password_confirmation' => '123456789',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonValidationErrors([
+                'password' => [
+                    'The password field confirmation does not match.'
+                ],
+            ]);
+    }
+
+    public function testUserCanonChangePasswordWhichContainsLessThanEightCharacters(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->putJson('/api/v1/password', [
+            'current_password' => 'password',
+            'password' => 'test',
+            'password_confirmation' => 'test',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors'])
+            ->assertJsonValidationErrors([
+                'password' => [
+                    'The password field must be at least 8 characters.'
+                ],
+            ]);
     }
 }
