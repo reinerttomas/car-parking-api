888f8e73cefa3b1e175298ede07bc7beee624342
Tomáš Reinert <reinerttomas@gmail.com>
2023-08-08 08:22:28 +0200

    fix guard

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use App\Models\User;
 
 if (! function_exists('user')) {
-    function user(string $guard = 'web'): User
+    function user(?string $guard = null): User
     {
         $user = auth($guard)->user();
 
