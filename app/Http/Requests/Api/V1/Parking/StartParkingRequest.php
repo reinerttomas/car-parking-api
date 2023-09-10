638e66c8c023ddc9eaf30a7aa85a811f3a31dccf
Tomáš Reinert <reinerttomas@gmail.com>
2023-09-10 16:12:03 +0200

    clear

--- app/Http/Requests/Api/V1/Parking/StartParkingRequest.php
+++ app/Http/Requests/Api/V1/Parking/StartParkingRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\Api\V1\Parking;
 
-use App\Http\Requests\DataPassedValidation;
 use Illuminate\Foundation\Http\FormRequest;
 
 final class StartParkingRequest extends FormRequest
